ed597116b29022c6c5c6a2f38b1cf5e217b97e60
Jieqiong Yu <yjieqiong@gmail.com>
2018-08-20 17:43:46 +0800

    Add map

---
+++ MAP/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#rbf745",
+		"white": "#ffffff",
+	}
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
