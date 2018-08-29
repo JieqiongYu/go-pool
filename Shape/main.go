bc7d708791832794e1cf1e037364ed893adab8ec
Jieqiong Yu <yjieqiong@gmail.com>
2018-08-29 13:39:00 +0800

    Interfaces

---
+++ Shape/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct {
+	height float64
+	base   float64
+}
+type square struct {
+	sideLength float64
+}
+
+func main() {
+	t := triangle{1.0, 2.0}
+	s := square{2.0}
+
+	printArea(t)
+	printArea(s)
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
+
+func (t triangle) getArea() float64 {
+	return t.base * t.height / 2
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
