ba5524caeda25c8fd59e27380d181b96602171e8
Jieqiong Yu <jieqiong-yu@mercari.com>
2019-03-12 20:44:00 +0900

    Add traffic-light

---
+++ Go-Basics/traffic-light/README.md
@@ -0,0 +1,2 @@
+# Traffic Light
+It's a demo for demonstrating the usage of grouping constants and `iota`. 
---
+++ Go-Basics/traffic-light/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const (
+	_ = iota
+	TrafficLightStateRedLight
+	TrafficLightStateGreenLight
+	TrafficLightStateYellowLight
+)
+
+func main() {
+	fmt.Println("Red Light State Code:", TrafficLightStateRedLight)
+	fmt.Println("Green Light State Code:", TrafficLightStateGreenLight)
+	fmt.Println("Yellow Light State Code:", TrafficLightStateYellowLight)
+}
